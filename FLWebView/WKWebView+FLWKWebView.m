b2455848e281ff07faddde07e6f19547b9092032
Steve Richey <srichey@floatlearning.com>
2014-11-21 16:45:23 -0600

    add FLWebViewProvider implementations to FLWKWebView category

--- FLWebView/WKWebView+FLWKWebView.m
+++ FLWebView/WKWebView+FLWKWebView.m
@@ -7,13 +7,49 @@
 //
 
 #import "WKWebView+FLWKWebView.h"
+#import <objc/runtime.h>
 
 @implementation WKWebView (FLWKWebView)
 
-- (void) setDelegateViews:(UIView<WKNavigationDelegate,WKUIDelegate> *)delegateView
+- (void) setDelegateViews: (UIView<WKNavigationDelegate,WKUIDelegate> *) delegateView
 {
     self.navigationDelegate = delegateView;
     self.UIDelegate = delegateView;
+}
+
+- (UIView *) getAsUIView
+{
+    return self;
+}
+
+- (NSURLRequest *) getRequest
+{
+    return self.request;
+}
+
+- (BOOL) getIsLoading
+{
+    return self.isLoading;
+}
+
+- (NSURL *) getCurrentURL
+{
+    return self.URL;
+}
+
+- (void) setScalePagesToFit: (BOOL) setPages
+{
+    return; // not supported in WKWebView
+}
+
+- (void) setRequest: (NSURLRequest *) request
+{
+    objc_setAssociatedObject(self, @selector(request), request, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+- (NSURLRequest *) request
+{
+    return objc_getAssociatedObject(self, @selector(request));
 }
 
 @end
