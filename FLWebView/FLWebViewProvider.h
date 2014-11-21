5e1e5b18cf33af21997385ab4358780e35dab7f8
Steve Richey <srichey@floatlearning.com>
2014-11-21 16:41:59 -0600

    documentation for FLWebViewProvider

--- FLWebView/FLWebViewProvider.h
+++ FLWebView/FLWebViewProvider.h
@@ -10,20 +10,38 @@
 
 /*
  * This class defines methods that FLUIWebView and FLWKWebView should implement in
- * order to work with FLBrowserViewController.
+ * order to work within our ViewController.
  */
 @protocol FLWebViewProvider <NSObject>
 
+/*
+ * Assign a delegate view for this webview.
+ */
 - (void) setDelegateViews: (UIViewController *) delegateView;
 
+/*
+ * Returns this as a UIView object, which can be useful for setting properties inherited from UIView.
+ */
 - (UIView *) getAsUIView;
 
+/*
+ * Return the active NSURLRequest of this webview.
+ */
 - (NSURLRequest *) getRequest;
 
+/*
+ * Load an NSURLRequest in the active webview.
+ */
 - (void) loadRequest: (NSURLRequest *) request;
 
+/*
+ * Returns true if loading, false otherwise.
+ */
 - (BOOL) getIsLoading;
 
+/*
+ * Returns true if it is possible to go back, false otherwise.
+ */
 - (BOOL) canGoBack;
 
 @end
