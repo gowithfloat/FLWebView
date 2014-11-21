23d3557aa2497a6c9e53ac3304d415d7ad9cdb02
Steve Richey <srichey@floatlearning.com>
2014-11-21 14:23:11 -0600

    Initial Commit

---
+++ FLWebView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FLWebView
+//
+//  Created by Steve Richey on 11/21/14.
+//  Copyright (c) 2014 Float Mobile Learning. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FLWebView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FLWebView
+//
+//  Created by Steve Richey on 11/21/14.
+//  Copyright (c) 2014 Float Mobile Learning. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
