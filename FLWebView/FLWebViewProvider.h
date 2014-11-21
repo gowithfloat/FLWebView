5f431a18f71f00305b47eea9be3d1dc2115565eb
Steve Richey <srichey@floatlearning.com>
2014-11-21 15:25:39 -0600

    update configuration

---
+++ FLWebView/FLWebViewProvider.h
@@ -0,0 +1,13 @@
+//
+//  FLWebViewProvider.h
+//  FLWebView
+//
+//  Created by Steve Richey on 11/21/14.
+//  Copyright (c) 2014 Float Mobile Learning. All rights reserved.
+//
+
+#ifndef FLWebView_FLWebViewProvider_h
+#define FLWebView_FLWebViewProvider_h
+
+
+#endif
