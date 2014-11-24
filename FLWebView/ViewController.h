baf2aff6cd5d9e4e1131f87af677c58cd99036dc
Steve Richey <srichey@floatlearning.com>
2014-11-24 16:35:53 -0600

    documentation

--- FLWebView/ViewController.h
+++ FLWebView/ViewController.h
@@ -6,12 +6,20 @@
 //  Copyright (c) 2014 Float Mobile Learning. All rights reserved.
 //
 
+// Needed for UIViewController, UIWebViewDelegate, and UIView
 #import <UIKit/UIKit.h>
+// Needed for WKNavigationDelegate and WKUIDelegate
 #import <WebKit/WebKit.h>
+// Used to define the webView property below
 #import "FLWebViewProvider.h"
 
+/**
+ * This is the view controller for the main interface.
+ * It also acts as a delegate for the FLWebView.
+ */
 @interface ViewController : UIViewController <UIWebViewDelegate, WKNavigationDelegate, WKUIDelegate>
 
+// The main WebView that is set up in the viewDidLoad method.
 @property (nonatomic) UIView <FLWebViewProvider> *webView;
 
 @end
