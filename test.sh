3966642c0311ab37ff51d698245b891ad43a9558
Steve Richey <srichey@floatlearning.com>
2014-11-24 17:23:31 -0600

    use simulator for building and testing

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xctool -project FLWebView.xcodeproj -scheme FLWebView build test -test-sdk iphonesimulator7.1
+xctool -project FLWebView.xcodeproj -scheme FLWebView build -sdk iphonesimulator test -test-sdk iphonesimulator
