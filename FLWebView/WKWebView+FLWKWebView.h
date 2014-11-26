6314a243d23818f85f241b22d6e2e703f04649d9
Steve Richey <srichey@floatlearning.com>
2014-11-26 11:16:59 -0600

    FLWKWebView documentation

--- FLWebView/WKWebView+FLWKWebView.h
+++ FLWebView/WKWebView+FLWKWebView.h
@@ -9,8 +9,14 @@
 #import <WebKit/WebKit.h>
 #import "FLWebViewProvider.h"
 
+/*
+ * This category extends WKWebView and conforms to the FLWebViewProvider protocol.
+*/
 @interface WKWebView (FLWKWebView) <FLWebViewProvider>
 
+/*
+ * Shorthand for setting WKUIDelegate and WKNavigationDelegate to the same class.
+*/
 - (void) setDelegateViews: (UIView<WKNavigationDelegate, WKUIDelegate> *) delegateView;
 
 @end
