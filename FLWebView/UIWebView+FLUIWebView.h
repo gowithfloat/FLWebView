fd9add47ae94d9521fde89fd17fb62d55ae2478c
Steve Richey <srichey@floatlearning.com>
2014-11-26 11:12:08 -0600

    UIWebView+FLUIWebView docs

--- FLWebView/UIWebView+FLUIWebView.h
+++ FLWebView/UIWebView+FLUIWebView.h
@@ -9,8 +9,14 @@
 #import <UIKit/UIKit.h>
 #import "FLWebViewProvider.h"
 
+/*
+ * This category extends UIWebView and conforms to the FLWebViewProvider protocol.
+*/
 @interface UIWebView (FLUIWebView) <FLWebViewProvider>
 
+/*
+ * Shorthand for setting UIWebViewDelegate to a class.
+*/
 - (void) setDelegateViews: (UIView<UIWebViewDelegate> *) delegateView;
 
 @end
