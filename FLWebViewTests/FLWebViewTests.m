6e19667d6c84d2723d2cd41475035405a12dcf39
Steve Richey <srichey@floatlearning.com>
2014-11-26 14:02:45 -0600

    add some basic unit tests

--- FLWebViewTests/FLWebViewTests.m
+++ FLWebViewTests/FLWebViewTests.m
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
+#import <WebKit/WebKit.h>
+#import "FLWebViewProvider.h"
+#import "ViewController.h"
+#import "UIWebView+FLUIWebView.h"
+#import "WKWebView+FLWKWebView.h"
+
+#define TEST_URL @"http://www.floatlearning.com"
 
 @interface FLWebViewTests : XCTestCase
 
@@ -15,15 +22,26 @@
 
 @implementation FLWebViewTests
 
-- (void)setUp {
-    [super setUp];
-}
-
-- (void)tearDown {
-    [super tearDown];
-}
-
-- (void)testExample {
+- (void) testExample
+{
+    // dummy check
     XCTAssertTrue(true);
+    
+    // create a UI View that conforms to FLWebViewProvider
+    UIView <FLWebViewProvider> *webView;
+    
+    // instantiate
+    if (NSClassFromString(@"WKWebView")) {
+        webView = [[WKWebView alloc] init];
+    } else {
+        webView = [[UIWebView alloc] init];
+    }
+    
+    XCTAssertNotNil(webView);
+    
+    [webView loadRequestFromString: TEST_URL];
+    
+    XCTAssertNotNil([webView request]);
+    XCTAssertNotNil([webView URL]);
 }
 @end
