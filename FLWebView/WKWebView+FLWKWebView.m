b46f8becb2afbce995ece1b60632bf3eda7ff8df
Steve Richey <srichey@floatlearning.com>
2014-11-26 11:20:30 -0600

    use method swizzling to override loadRequest in FLWKWebView

--- FLWebView/WKWebView+FLWKWebView.m
+++ FLWebView/WKWebView+FLWKWebView.m
@@ -44,7 +44,16 @@
     objc_setAssociatedObject(self, @selector(request), request, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
+/*
+ * This is swizzled in place of loadRequest in the load method.
+ * Just updates the request reference before requesting.
+*/
+- (void) altLoadRequest: (NSURLRequest *) request
 {
+    self.request = request;
+    
+    // Since we swizzled with loadRequest, this will actually call the original loadRequest.
+    [self altLoadRequest: request];
 }
 
 /*
@@ -63,7 +72,38 @@
     return; // not supported in WKWebView
 }
 
+/*
+ * This class method is called when the runtime is loading.
+ * We override this method to replace (swizzle) the loadRequest method with altLoadRequest.
+ * That way, every time another class calls loadRequest, it'll actually call altLoadRequest.
+*/
++ (void) load
 {
+    static dispatch_once_t onceToken;
+    
+    // We want to make sure this is only done once!
+    dispatch_once(&onceToken, ^{
+        Class class = [self class];
+        
+        // Get the representation of the method names to swizzle.
+        SEL originalSelector = @selector(loadRequest:);
+        SEL swizzledSelector = @selector(altLoadRequest:);
+        
+        // Get references to the methods to swizzle.
+        Method originalMethod = class_getInstanceMethod(class, originalSelector);
+        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+        
+        // Attempt to add the new method in place of the old method.
+        BOOL didAddMethod = class_addMethod(class, originalSelector, method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod));
+        
+        // If we succeeded, put the old method in place of the new method.
+        if (didAddMethod) {
+            class_replaceMethod(class, swizzledSelector, method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod));
+        } else {
+            // Otherwise, just swap their implementations.s
+            method_exchangeImplementations(originalMethod, swizzledMethod);
+        }
+    });
 }
 
 @end
